142cb98b65e8f044dfc41968f77ec9547d7c0d67
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-09-09 19:48:28 -0300

    Testes de Serviço. Seção 4 - Aula 18. Pendente estabilizar.

--- cursoMassa/src/test/java/br/ce/wcaquino/estrategia3/GeradorMassas.java
+++ cursoMassa/src/test/java/br/ce/wcaquino/estrategia3/GeradorMassas.java
@@ -8,10 +8,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.github.javafaker.Faker;
 
+import br.ce.wcaquino.entidades.Conta;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.service.ContaService;
+import br.ce.wcaquino.service.UsuarioService;
+
 public class GeradorMassas {
 
 	private static FirefoxDriver driver;
 	public static final String CHAVE_CONTA_SB = "CONTA_SB";
+	public static final String CHAVE_CONTA = "CONTA";
 
 	
 	public void gerarContaSeuBarriga() throws ClassNotFoundException, SQLException {
@@ -39,11 +45,25 @@ public class GeradorMassas {
 		new MassaDAOImpl().inserirMassa(CHAVE_CONTA_SB, registro);
 
 	}
+	
+	public void gerarConta() throws Exception {
+		
+		Faker faker = new Faker();
+		ContaService service = new ContaService();
+		UsuarioService userService = new UsuarioService();
+		
+		Usuario usuarioGlobal = new Usuario(faker.name().fullName(), faker.internet().emailAddress(),
+				faker.internet().password());
+		Usuario Usuariosalvo = userService.salvar(usuarioGlobal);
+		Conta conta = new Conta(faker.superhero().name(), Usuariosalvo);
+		service.salvar(conta);
+		new MassaDAOImpl().inserirMassa(CHAVE_CONTA, conta.getNome());
+	}
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws Exception {
 		GeradorMassas gerador = new GeradorMassas();
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 2; i++) {
 			gerador.gerarContaSeuBarriga();
 		}
 //		String massa = new MassaDAOImpl().obterMassa(CHAVE_CONTA_SB);

2da1b6233211bbc1c4fc3acfaa8923097c0bf9a5
droidtrail <31170024+droidtrail@users.noreply.github.com>
2018-09-22 10:34:33 -0300

    Importando massa para o banco. Seção 9. Aula 33.

--- cursoMassa/src/test/java/dbunit/ImportExport.java
+++ cursoMassa/src/test/java/dbunit/ImportExport.java
@@ -1,24 +1,33 @@
 package dbunit;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.sql.SQLException;
 
-import org.dbunit.DatabaseUnitException;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 
 import br.ce.wcaquino.dao.utils.ConnectionFactory;
 
 public class ImportExport {
-	public static void main(String[] args) throws ClassNotFoundException, DatabaseUnitException, SQLException, IOException {
+	public static void main(String[] args) throws Exception{
+
+		//exportarBanco();
+		DatabaseConnection dbConn = new DatabaseConnection(ConnectionFactory.getConnection());
+		FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
+		IDataSet dataSet = builder.build(new FileInputStream("massas" + File.separator + "entrada.xml"));
+		DatabaseOperation.CLEAN_INSERT.execute(dbConn, dataSet);
+
+	}
+
+	private static void exportarBanco() throws Exception{
 		
 		DatabaseConnection dbConn = new DatabaseConnection(ConnectionFactory.getConnection());
 		IDataSet dataSet = dbConn.createDataSet();
 		FileOutputStream fos = new FileOutputStream("massas" + File.separator + "saida.xml");
 		FlatXmlDataSet.write(dataSet, fos);
-	
 	}
 }
